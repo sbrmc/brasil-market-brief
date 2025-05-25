f98a23237287a84c7335ae80d2bdefce80670756
SilBrma <Silviobrma95@gmail.com>
2025-05-25 20:37:01 -0300

    ajuste no cabeçalho

---
+++ reports.js
@@ -0,0 +1,17 @@
+// reports.js
+export const reports = [
+  {
+    date: '2025-05-22',                     // dia de referência
+    title: 'Panorama Pré-Mercado – 22/05/2025',
+    html: 'relatorios/2025-05-22.html',     // caminho relativo
+    audio: 'audios/2025-05-22.mp3'
+  },
+  {
+    date: '2025-05-23',                     // dia de referência
+    title: 'Panorama Pré-Mercado – 23/05/2025',
+    html: 'relatorios/2025-05-23.html',     // caminho relativo
+    audio: 'audios/2025-05-23.mp3'
+  },
+
+  // acrescente novos objetos aqui, sempre o mais recente no topo
+];
